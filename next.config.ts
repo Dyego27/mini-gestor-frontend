7d9cc8640c5d1fb780a6417ce8bae875c352e2b3
Dyego27 <dyegoluis123@gmail.com>
2025-11-12 17:00:37 -0300

    Hierarquia das pastas feitas !

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/v1/:path*",
+
+        destination: "http://localhost:8080/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
