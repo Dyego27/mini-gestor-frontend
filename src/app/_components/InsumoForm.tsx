484849212f129b1dd6253e9c6cdb607d749a36fb
Dyego27 <dyegoluis123@gmail.com>
2025-11-13 15:47:58 -0300

    adicionando component input

--- src/app/_components/InsumoForm.tsx
+++ src/app/_components/InsumoForm.tsx
@@ -1,7 +1,25 @@
-"use client"
-
-export default function InsumoForm(){
-    return(
-
-    );
+"use client";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+export default function InsumoForm() {
+  return (
+    <Card className="container mx-auto">
+      <CardHeader>
+        <CardTitle>Insumo Forms</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p>Card Content</p>
+      </CardContent>
+      <CardFooter>
+        <p>Card Footer</p>
+      </CardFooter>
+    </Card>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 "use client";
 
+import InsumoForm from "./_components/InsumoForm";
 import InsumoTable from "./_components/InsumoTable";
 
 export default function HomePage() {
-  return <InsumoTable />;
+  return (
+    <main>
+      <InsumoTable />
+      <InsumoForm />
+    </main>
+  );
 }
